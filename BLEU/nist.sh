6a43d2d0146d6ad4f2bcaabfdc9410e30bb14386
Glaceon31 <grit31@126.com>
2016-06-30 09:55:55 +0800

    nist

--- BLEU/nist.sh
+++ BLEU/nist.sh
@@ -2,4 +2,4 @@ TRANS=$1
 REF=$2
 Y=$3
 
-perl multi-bleu.perl -lc $REF/nist$Y/nist$Y.en0 $REF/nist$Y/nist$Y.en1 $REF/nist$Y/nist$Y.en2 $REF/nist$Y/nist$Y.en3 < $TRANS
+perl ~/NLPtools/BLEU/multi-bleu.perl -lc $REF/nist$Y/nist$Y.en0 $REF/nist$Y/nist$Y.en1 $REF/nist$Y/nist$Y.en2 $REF/nist$Y/nist$Y.en3 < $TRANS
